470a37bd61280b02135576316964c89373554691
Brian <karabinchak.brian@gmail.com>
2021-05-07 09:52:46 -0400

    Registering tables to admin site

--- thoughtlogger/admin.py
+++ thoughtlogger/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Activity, ThoughtCategory, Thought
+
+table_to_register = [Activity, ThoughtCategory, Thought]
+[admin.site.register(table) for table in table_to_register]
