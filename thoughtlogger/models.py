142ce9e7ba457158d29d9318b87ce4b98c3e82a3
Brian <karabinchak.brian@gmail.com>
2021-05-08 18:44:25 -0400

    New models for Values, Milestones, and selected values but that may change

--- thoughtlogger/models.py
+++ thoughtlogger/models.py
@@ -68,7 +68,27 @@ class Errands(models.Model):
     def __str__(self):
         return self.errand_text
 
+class Value(models.Model):
+    value_text = models.CharField(max_length=200)
+
+# milestones in values that you've reached
+# kind of like proof that 
+class Milestone(models.Model):
+    # so we can say hey you have munachieved milestones in this model
+    value = models.ForeignKey(Value, on_delete=models.PROTECT)
+    milestone_text = models.CharField(max_length=300)
+    complete_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f"{self.value.value_text}: {slef.milestone_text}"
+
 class Routines(models.Model):
     routine_text = models.CharField(max_length=200)
     description = models.TextField()
 #    days = models.ManyToManyField(DayOfTheWeekField)
+    milestone = models.ForeignKey(Milestone, null=True, on_delete=models.PROTECT)
+
+class SelectedValue(models.Model):
+    value = models.ForeignKey(Value, on_delete=models.PROTECT)
+    importance = models.TextField()
+    routines = models.ForeignKey(Routines, on_delete=models.PROTECT)
