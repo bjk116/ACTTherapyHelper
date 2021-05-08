241c954d423bcc937ecbf636f310ba7cd17c8713
Brian <karabinchak.brian@gmail.com>
2021-05-08 10:35:40 -0400

    Registering more models for admin

--- thoughtlogger/admin.py
+++ thoughtlogger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Activity, ThoughtCategory, Thought
+from .models import Activity, ThoughtCategory, Thought, Errands, Routines
 
-table_to_register = [Activity, ThoughtCategory, Thought]
+table_to_register = [Activity, ThoughtCategory, Thought, Errands, Routines]
 [admin.site.register(table) for table in table_to_register]
