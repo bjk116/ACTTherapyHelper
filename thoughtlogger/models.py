67df9c0e551a2ab7ac6c62e14aa2b48b539e24e6
Brian <karabinchak.brian@gmail.com>
2021-05-08 10:36:05 -0400

    New Errands and Routine tables, attempt at days of the week

--- thoughtlogger/models.py
+++ thoughtlogger/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
+# DAYS_OF_WEEK = (
+#     (0, 'Monday'),
+#     (1, 'Tuesday'),
+#     (2, 'Wednesday'),
+#     (3, 'Thursday'),
+#     (4, 'Friday'),
+#     (5, 'Saturday'),
+#     (6, 'Sunday'),
+# )
 
+# class Days(models.Model):
+#     day = models.CharField(max_length=8, choices=DAYS_OF_WEEK)
+
+# DAY_OF_THE_WEEK = {
+#     0 : ('Monday'),
+#     1 : ('Tuesday'),
+#     2 : ('Wednesday'),
+#     3 : ('Thursday'),
+#     4 : ('Friday'),
+#     5 : ('Saturday'), 
+#     6 : ('Sunday'),
+# }
+
+# class DayOfTheWeekField(models.CharField):
+#     def __init__(self, *args, **kwargs):
+#         kwargs['choices']=tuple(sorted(DAY_OF_THE_WEEK.items()))
+#         kwargs['max_length']=1 
+#         super(DayOfTheWeekField,self).__init__(*args, **kwargs)
 
 # Create your models here.
 class Activity(models.Model):
@@ -27,3 +54,21 @@ class Thought(models.Model):
         thought = f"{self.thought_text[:100]}..."
         # TODO: figure out way to display first couple of thought categories selected
         return f"{dt}: {thought}"
+
+class Errands(models.Model):
+    errand_text = models.CharField(max_length=200)
+    description = models.TextField(null=True)
+    create_date = models.DateTimeField(auto_now_add=True, blank=True)
+    due_date = models.DateTimeField()
+    # next_attempt_date will be used for notification/alarms
+    # and what will be reset if the user doesn't do it
+    next_attempt_date = models.DateTimeField(null=True)
+    complete_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.errand_text
+
+class Routines(models.Model):
+    routine_text = models.CharField(max_length=200)
+    description = models.TextField()
+#    days = models.ManyToManyField(DayOfTheWeekField)
