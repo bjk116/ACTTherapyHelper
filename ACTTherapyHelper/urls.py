2299cabbde5ac071fc30073a53cf146a5349f1f9
Brian <karabinchak.brian@gmail.com>
2021-05-08 18:43:13 -0400

    Made thought logger default application

--- ACTTherapyHelper/urls.py
+++ ACTTherapyHelper/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('thoughts/', include('thoughtlogger.urls')),
+    path('', include('thoughtlogger.urls')),
     path('admin/', admin.site.urls),
 ]
