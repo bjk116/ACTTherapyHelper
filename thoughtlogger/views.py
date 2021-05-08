c155ab746d52fd44094944e994fd05b9c2863127
Brian <karabinchak.brian@gmail.com>
2021-05-08 10:36:44 -0400

    Initial views for CRUD operations on errands and routine models

--- thoughtlogger/views.py
+++ thoughtlogger/views.py
@@ -1,9 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Errands, Routines, Thought
 
 # Create your views here.
 def index(request):
     """
     Initial page the user will see
     """
-    return HttpResponse("Hello ACT User")
+    errands = Errands.objects.order_by('-due_date')[:10]
+    thoughts = Thought.objects.order_by('-create_date')[:10]
+    context = {'upcoming_errands':errands, 'recent_thoughts':thoughts}
+    return render(request, 'thoughtlogger/index.html', context)
+
+def add_errand(request):
+    """
+    Add errand
+    """
+    pass
+
+def edit_errand(request):
+    """
+    Edit errand
+    """
+    pass
+
+def delete_errand(request):
+    """
+    Delete errand
+    """
+    pass
+
+def add_repeating_routine(request):
+    """
+    Add repeating to routine
+    """
+    pass
+
+def edit_repeating_routine(request):
+    """
+    Edit repating to routine
+    """
+    pass
+
+def delete_repeating_routine(request):
+    """
+    Delete repeating to routine
+    """
+    pass
+
+def log_present_moment_thought(requests):
+    """
+    Log thoughts about what they are doing in the present moment.
+    """
+    pass
+
+def log_routine_thought(requests):
+    """'
+    Log thoughts related to daily/repeating to do items.
+    """
+    pass
+
+def log_errand_thought(requests):
+    """
+    Log thoughts related to one-shot errands.
+    """
+    pass
