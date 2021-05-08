2bc6f4cda87a0383966f387780a0bdcaf525c528
Brian <karabinchak.brian@gmail.com>
2021-05-08 18:45:36 -0400

    Added errand edit view

--- thoughtlogger/urls.py
+++ thoughtlogger/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'thoughtlogger'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('edit/errand/<int:errand_id>/', views.edit_errand, name='edit_errand'),
 ]
