5d92c161aa7e0499789a2466f09dc06130a760d1
Brian <karabinchak.brian@gmail.com>
2021-05-08 18:43:52 -0400

    Added models to admin page

--- thoughtlogger/admin.py
+++ thoughtlogger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Activity, ThoughtCategory, Thought, Errands, Routines
+from .models import Activity, ThoughtCategory, Thought, Errands, Routines, Value, Milestone,  SelectedValue
 
-table_to_register = [Activity, ThoughtCategory, Thought, Errands, Routines]
+table_to_register = [Activity, ThoughtCategory, Thought, Errands, Routines, Value, Milestone, SelectedValue]
 [admin.site.register(table) for table in table_to_register]
